eafb7d1f8a19a966e504c19e37fa414f8427365b
vvmsecured <vvmspace@gmail.com>
2018-05-04 17:30:06 +0500

    класс FMTwitter для дальнейшего использования с возможностью подмены стандартных функций

---
+++ app/FMTwitter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vvm
+ * Date: 04.05.18
+ * Time: 16:21
+ */
+
+namespace App;
+use Thujohn\Twitter\Facades\Twitter;
+
+class FMTwitter extends Twitter
+{
+    // класс FMTwitter для дальнейшего использования с возможностью подмены стандартных функций
+}
